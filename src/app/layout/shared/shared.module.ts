335f10e76f75a87c6f313039aa1531f3d3bb45d3
hzjangular <2962975318qq.com>
2018-07-27 16:47:45 +0800

    共享模块1.0

--- src/app/layout/shared/shared.module.ts
+++ src/app/layout/shared/shared.module.ts
@@ -1,15 +1,72 @@
+///<reference path="../../../../node_modules/@angular/material/autocomplete/typings/autocomplete-module.d.ts"/>
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {
+  MatAutocompleteModule,
+  MatButtonModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDialogModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSlideToggleModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule
+} from '@angular/material';
 
 @NgModule({
   imports: [
-    CommonModule
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatDialogModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSlideToggleModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule
   ],
   declarations: [
 
   ],
   exports: [
-    CommonModule
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatDialogModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSlideToggleModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule
   ]
 })
 export class SharedModule { }
