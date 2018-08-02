88bf628545b34f2edc8d1b9eeba18e1cd2bb7e72
hzjangular <2962975318qq.com>
2018-08-02 17:01:39 +0800

    问答页面

--- src/app/layout/answer/answer-routing.module.ts
+++ src/app/layout/answer/answer-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ProblemHomeComponent} from './problem/problem-home/problem-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ProblemHomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
